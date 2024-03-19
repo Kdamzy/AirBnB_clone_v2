a876a0ccdea8b5b701a8849bafffde1b7af1dd29
Destiny Saturday <yiradesat@gmail.com>
2024-03-19 17:52:11 +0100

    feat: add public method to delete object and return one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a list of object of one type of class"""
+        if cls:
+            new_dict = {}
+            for key, val in self.__objects.items():
+                if cls.__name__ in key:
+                    new_dict[key] = val
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it’s inside"""
+        if obj:
+            key = obj.__class__.__name__ + "." + obj.id
+            if key in self.__objects:
+                del self.__objects[key]

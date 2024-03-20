c92fc308fbd0693674384de82ec8d2a4d17413a4
Destiny Saturday <yiradesat@gmail.com>
2024-03-20 18:00:59 +0100

    feat: implementing db_storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
-from sqlalchemy import Column
+
+from datetime import datetime, timezone
+from sqlalchemy import Column, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), primary_key=True, nullable=False)
+            self.created_at = Column(
+                DateTime, default=datetime.now(timezone.utc), nullable=False
+            )
+            self.updated_at = Column(
+                DateTime, default=datetime.now(timezone.utc), nullable=False
+            )
         else:
+            if 'name' in kwargs:
+                self.name = kwargs['name']
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
@@ -33,9 +39,13 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+        
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -45,4 +55,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models import storage
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """
+            Getter attribute that returns the list of City instances
+            with state_id equals to the current State.id
+            """
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list

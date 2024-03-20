9417a47ddb5440bf18a1d06ee7d87e02c493883f
Kdamzy <kolajidedamilare@gmail.com>
2024-03-20 20:42:25 +0100

    initial commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage db storage for hbnb clone"""
 from os import getenv
+import os
 
 from sqlalchemy import create_engine
 from models.base_model import Base
 from models.state import State
 from models.city import City
 from models.user import User
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from sqlalchemy.orm import sessionmaker
@@ -20,20 +22,32 @@ class DBStorage:
     
     def __init__(self):
         """Create the engine"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        dbase = os.getenv('HBNB_MYSQL_DB')
+        dbase_url = 'mysql+mysqldb://{}:{}@{}:3306/{}'.format(
+                user, password, host, dbase
+        )
+        self.__engine = create_engine(
+            dbase_url,
+            pool_pre_ping=True
+        )
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
             
     def all(self, cls=None):
         """query on the current database session"""
 
-        session = scoped_session(sessionmaker(bind=self.__engine))
-        classes = [State, City, User, Place, Review]
+        session = scopes_session(sessionmaker(bind=self.__engine))
+        objs = dict()
+        all_object = (User, State, City, Amenity, Place, Review)
+        if cls is None:
+            for object_type in all_object:
+                query = self.session.query(object_type)
+                for obj in query.all():
+                    key + f"{object.__class__.__name__} . {obj.id}"
+                    objs[key] = obj
         if cls:
             if type(cls) == str:
                 cls = eval(cls)
